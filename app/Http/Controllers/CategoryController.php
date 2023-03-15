8125ac1db761a3f8ed38ab2a5f3f2854e9744536
fern1193458 <fvazques120@gmail.com>
2023-03-14 20:40:17 -0500

    controllers

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,64 +2,113 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        // $categories = Category::paginate(10);
+        $categories = Category::all();
+        dd($categories);
+        // Retornar vista inyectando todos las categorias
+
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        // Retornar la vista elements.categories.create
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $category = new Category;
+
+        $category->name = $request->name;
+        $category->description = $request->description;
+
+        if($category->save()){
+            dd($category);
+            //Retornar la vista
+        }
+
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show(Category $category)
+    public function show($id)
     {
-        //
+        $category = Category::find($id);
+        dd($category);
+        // Retornar la vista
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function edit(Category $category)
+    public function edit($id)
     {
-        //
+        $category = Category::find($id);
+        //Retorna la vista con el formulario de edición del usuario
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, $id)
     {
-        //
+        $category = Category::find($id);
+
+        $category->name = $request->name;
+        $category->description = $request->description;
+
+        if($category->save()){
+            dd($category);
+            //Retornar la vista
+        }
+
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(Category $category)
+    public function destroy($id)
     {
-        //
+        $category = Category::find($id);
+
+        if($category->delete()){
+            // Retorne la vista index con el mensaje que pudo eliminar el elemento exitosamente
+        }
     }
 }
--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,64 +2,135 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Role;
+
 use Illuminate\Http\Request;
 
-class RoleController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        // $user = User::paginate(10);
+        $users = User::all();
+        // dd($users);
+        return view('test')->with('users',$users);
+        // Retornar vista inyectando todos los usuarios
+
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        $roles = Role::all();
+        // Retornar la vista elements.users.create
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $user = new User;
+
+        $user->fullname = $request->fullname;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        if($request->hasFile('photo')){
+            $file = time().'.'.$request->image->extension();
+            $request->image->move(public_path('images/profiles', $file));
+            $user->photo = 'images/profiles/'.$file;
+        }
+
+        $user->password = bcrypt($request->password);
+        $user->role_id = $request->role_id;
+
+        if($user->save()){
+            dd($user);
+            //Retornar la vista
+        }
+
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show(Role $role)
+    public function show($id)
     {
-        //
+        $user = User::find($id);
+        dd($user);
+        // Retornar la vista
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function edit(Role $role)
+    public function edit($id)
     {
-        //
+        $user = User::find($id);
+        //Retorna la vista con el formulario de edición del usuario
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Role $role)
+    public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+
+        $user->fullname = $request->fullname;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        if($request->hasFile('photo')){
+            $file = time().'.'.$request->image->extension();
+            $request->image->move(public_path('images/profiles', $file));
+            $user->photo = 'images/profiles/'.$file;
+        }
+
+        $user->password = bcrypt($request->password);
+        $user->role_id = $request->role_id;
+
+        if($user->save()){
+            dd($user);
+            //Retornar la vista
+        }
+
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(Role $role)
+    public function destroy($id)
     {
-        //
+        $user = User::find($id);
+
+        if($user->delete()){
+            // Retorne la vista index con el mensaje que pudo eliminar el elemento exitosamente
+        }
     }
 }
